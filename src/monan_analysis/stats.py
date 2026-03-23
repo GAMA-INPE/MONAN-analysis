47eb95c3418df2852768c22dd8f96ee997eaa0df
Guilherme Torres Mendonca <guilherme.mendonca@inpe.br>
2026-03-23 15:47:55 +0000

    stats metrics descriptions

--- src/monan_analysis/stats.py
+++ src/monan_analysis/stats.py
@@ -28,7 +28,22 @@ def example_function_stats():
     print ("this is a function imported from the stats.py module.")
 
 def bias(predictions, observations):
-    """Calculate the bias between predictions and observations while preserving dataset structure."""
+    """
+    Calculate the bias between predictions and observations while preserving xarray dataset structure.
+    
+    Bias may be defined as the 'systematic difference' between a predicted variable and the corresponding
+    observed variable, where 'systematic' means that the difference is performed for averages of the 
+    variable over a spatial and time domain (1,2). The spatial and time averaging of the variable 
+    is left to the user. This function only calculates the difference
+
+    bias[var] = predictions[var] - observations[var]
+
+    References:
+    1. Maraun et al, 2016, "Towards process-oriented bias adjustment of climate change simulations", 
+    Nature Climate Change, v 7, n 11, 764-773
+    2. Ehret et al, 2012, "HESS Opinions: Should we apply bias correction to global and regional 
+    climate model data?", Hydrology and Earth System Sciences, v 16, n 9, 3391-3404
+    """
     # Ensure inputs are xarray Datasets
     if not isinstance(predictions, xr.Dataset) or not isinstance(observations, xr.Dataset):
         raise TypeError("Both predictions and observations must be xarray Datasets.")
@@ -41,7 +56,24 @@ def bias(predictions, observations):
     return result
 
 def bias_mean(predictions, observations):
-    """Calculate the mean bias between predictions and observations."""
+    """
+    Calculate the mean bias between predictions and observations.
+    
+    Bias may be defined as the 'systematic difference' between a predicted variable and the corresponding
+    observed variable, where 'systematic' means that the difference is performed for averages of the 
+    variables over a spatial and time domain (1,2). The spatial and time averaging of the variables 
+    is left to the user. This function calculates the difference
+
+    bias[var] = predictions[var] - observations[var]
+
+    and then takes its overall mean over the bias[var] array.
+
+    References:
+    1. Maraun et al, 2016, "Towards process-oriented bias adjustment of climate change simulations", 
+    Nature Climate Change, v 7, n 11, 764-773
+    2. Ehret et al, 2012, "HESS Opinions: Should we apply bias correction to global and regional 
+    climate model data?", Hydrology and Earth System Sciences, v 16, n 9, 3391-3404 
+    """
     # Ensure inputs are xarray Datasets
     if not isinstance(predictions, xr.Dataset) or not isinstance(observations, xr.Dataset):
         raise TypeError("Both predictions and observations must be xarray Datasets.")
@@ -54,7 +86,18 @@ def bias_mean(predictions, observations):
     return result
 
 def relative_error(predictions, observations):
-    """Calculate the relative error between predictions and observations."""
+    """
+    Calculate the relative error between predictions and observations.
+    
+    Relative error is here defined as
+
+    relative_error = |predictions - observations| / |observations|,
+
+    assuming observations are the true value of the variable, and predictions are an approximation (1).
+
+    Reference:
+    1. Burden and Faires, Numerical Analysis, 9th edition, 2011
+    """
     if not isinstance(predictions, xr.Dataset) or not isinstance(observations, xr.Dataset):
         raise TypeError("Both predictions and observations must be xarray Datasets.")
     
@@ -65,7 +108,19 @@ def relative_error(predictions, observations):
     return result
 
 def relative_error_mean(predictions, observations):
-    """Calculate the mean relative error between predictions and observations."""
+    """
+    Calculate the mean relative error between predictions and observations.
+    
+    Relative error is here defined as
+
+    relative_error = |predictions - observations| / |observations|,
+
+    assuming observations are the true value of the variable, and predictions are an approximation (1).
+    This function calculates the mean of the relative_error array defined above.
+
+    Reference:
+    1. Burden and Faires, Numerical Analysis, 9th edition, 2011
+    """
     if not isinstance(predictions, xr.Dataset) or not isinstance(observations, xr.Dataset):
         raise TypeError("Both predictions and observations must be xarray Datasets.")
     
@@ -76,12 +131,55 @@ def relative_error_mean(predictions, observations):
     return result
 
 def rmse(predictions, observations):
-    """Calculate the root mean square error between predictions and observations."""
+    """
+    Calculate the root mean square error between predictions and observations.
+    
+    The root mean squared error is here defined as the root of the mean squared error as defined in (1), i.e.
+
+    RMSE = sqrt(1/n sum_{i=1}^{n} (prediction_i - observation_i)**2) 
+
+    Reference:
+    1. Jolliffe and Stephenson, Forecast Verification: A Practitioner's Guide in Atmospheric Science, 2003
+    """
     if not isinstance(predictions, xr.Dataset) or not isinstance(observations, xr.Dataset):
         raise TypeError("Both predictions and observations must be xarray Datasets.")
     
     result = predictions.copy()
     for var in predictions.data_vars:
         result[var] = (((predictions[var] - observations[var]) ** 2).mean()) ** 0.5
+    
+    return result
+
+def anomaly_correlation_coefficient(predictions, observations):
+    """
+    Calculate the anomaly correlation coefficient between predictions and observations.
+    
+    The anomaly correlation coefficient is here defined as the mean of the product of the anomalies 
+    of the predictions and observations, divided by the product of the standard deviations of the 
+    anomalies of the predictions and observations. This definition follows roughly that of (1), except
+    that in (1) the anomalies are calculated by subtracting the climatological mean of the variable
+    and then again its spatial mean, while here we subtract only the mean of the variable over the
+    whole array. The user is expected to calculate the anomalies as she/he sees fit before calling 
+    this function.
+
+    Mathematically, we define
+
+    ACC = (pred_anom * obs_anom).mean() / ((pred_anom ** 2).mean() ** 0.5 * (obs_anom ** 2).mean() ** 0.5),
+
+    where
+    pred_anom = predictions - predictions.mean()
+    obs_anom = observations - observations.mean()
+
+    Reference:
+    1. Jolliffe and Stephenson, Forecast Verification: A Practitioner's Guide in Atmospheric Science, 2003
+    """
+    if not isinstance(predictions, xr.Dataset) or not isinstance(observations, xr.Dataset):
+        raise TypeError("Both predictions and observations must be xarray Datasets.")
+    
+    result = predictions.copy()
+    for var in predictions.data_vars:
+        pred_anom = predictions[var] - predictions[var].mean()
+        obs_anom = observations[var] - observations[var].mean()
+        result[var] = (pred_anom * obs_anom).mean() / ((pred_anom ** 2).mean() ** 0.5 * (obs_anom ** 2).mean() ** 0.5)
     
     return result
