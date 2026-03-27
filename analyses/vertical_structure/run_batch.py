fe7c01b47576b881ea521d94ee4497b400c49aee
Guilherme Torres Mendonca <guilherme.mendonca@inpe.br>
2026-03-27 21:24:57 +0000

    wip

--- analyses/vertical_structure/run_batch.py
+++ analyses/vertical_structure/run_batch.py
@@ -4,8 +4,8 @@ run_single.py
 
 Description
 -----------
-Runs vertical structure analysis for a single date and single time window defined in 
-vertical_structure_src/vertical_structure.config.py.
+Runs vertical structure analysis for a single date and single time window, defined in 
+vertical_structure_src/vertical_structure_config.py.
 """
 
 import vertical_structure_src.vertical_structure_main as vs_main
--- analyses/vertical_structure/run_single.py
+++ analyses/vertical_structure/run_single.py
@@ -5,7 +5,7 @@ run_single.py
 Description
 -----------
 Runs vertical structure analysis for a single date and single time window defined in 
-vertical_structure_src/vertical_structure.config.py.
+vertical_structure_src/vertical_structure_config.py.
 """
 
 import vertical_structure_src.vertical_structure_main as vs_main
