correcting acc calculation again; checks for two days looking reasonable

analyses/vertical_structure/vertical_structure_src/vertical_structure_aux.py
@@ -1023,7 +1023,7 @@ def concatenate_var_datasets(date_list,time_window):
     if vs_config.SEL_VERBOSE_LEVEL >= 1:
         print ("Concatenating variable datasets for time window", time_window)
     ds_var_prediction_concat = xr.open_mfdataset(var_prediction_filepaths, combine="nested", concat_dim="Time")
-    ds_var_ref_concat = xr.open_mfdataset(var_prediction_filepaths, combine="nested", concat_dim="Time")
+    ds_var_ref_concat = xr.open_mfdataset(var_ref_filepaths, combine="nested", concat_dim="Time")
     if vs_config.SEL_VERBOSE_LEVEL >= 1:
         print ("Done concatenating variable datasets for time window", time_window)
     # Save concatenated datasets in nc file
@@ -1188,22 +1188,16 @@ def calculate_multi_time_metrics(time_window):
             )
 
         elif multi_time_metric == "anomaly_correlation_coefficient_standard":
-            # # Interpolate climatology to ref grid
-            # interpolate_prediction_ref(
-            #     ds_prediction_model_filepath=vs_config.FILEPATH_CLIMATOLOGY,
-            #     ds_ref_data_filepath=ds_var_ref_concat,
-            #     output_nc=vs_config.DIR_INPUT_PROCESSED+f"/climatology_mapped_to_ref_{vs_config.REFERENCE_DATA}.nc",
-            #     force_interpolation='y'
-            # )
-            # Read mapped climatology
-            #ds_climatology = xr.open_dataset(vs_config.DIR_INPUT_PROCESSED+f"/climatology_mapped_to_ref_{vs_config.REFERENCE_DATA}.nc", engine="netcdf4")
+            # Get month for calculation
+            month_MM = utils.get_MM_str_from_YYYYMMDDHH_str(date_string=vs_config.DATE_INIT)
+            # Get climatology dataset
             ds_climatology = xr.open_dataset(vs_config.FILEPATH_CLIMATOLOGY, engine="netcdf4")
-
+            # Compute anomaly correlation coefficient for the specified month
             ds_acc = stats.anomaly_correlation_coefficient_standard(
-                var="zgeo", 
-                predictions_monthly=ds_var_prediction_concat.mean(dim="Time", keep_attrs=True).sel(level="50000"),
-                observations_monthly=ds_var_ref_concat.mean(dim="Time", keep_attrs=True).sel(level="50000"),
-                climatology_monthly=ds_climatology.sel(level="50000", Time=7)
+                predictions=ds_var_prediction_concat,
+                observations=ds_var_ref_concat,
+                climatology=ds_climatology,
+                month_MM = month_MM
             )
 
             acc_filepath = (

exploratory/gtm_acc/check_acc_standard.py
@@ -8,16 +8,16 @@ DIR_MONAN_PREOP = "/lustre/projetos/ioper/models/MONAN-WorkFlow-OPER/MONAN_PRE_O
 DIR_GFS_ANALYSIS = "/lustre/projetos/monan_gam/andre.lyra/NetCDFs/vert_struct/GFS"
 DIR_GFS = "/lustre/projetos/monan_atm/guilherme.mendonca/scratch/data/GFS"
 DIR_CARTOPY_DATA = "/lustre/projetos/monan_gam/andre.lyra/cartopy"
-DIR_OUTPUT = f"/lustre/projetos/monan_atm/guilherme.mendonca/MONAN-analysis/analyses/vertical_structure/results/test_acc_standard_gfs_vs_gfs_analysis/output"
+DIR_OUTPUT = f"/lustre/projetos/monan_atm/guilherme.mendonca/MONAN-analysis/analyses/vertical_structure/results/output"
 DIR_OUTPUT_FIGS = f"{DIR_OUTPUT}/figs"
 DIR_OUTPUT_DATA = f"{DIR_OUTPUT}/data"
-DIR_INPUT = f"/lustre/projetos/monan_atm/guilherme.mendonca/MONAN-analysis/analyses/vertical_structure/results/test_acc_standard_gfs_vs_gfs_analysis/input"
+DIR_INPUT = f"/lustre/projetos/monan_atm/guilherme.mendonca/MONAN-analysis/analyses/vertical_structure/results/input"
 DIR_INPUT_INTERMEDIATE = f"{DIR_INPUT}/intermediate"
 DIR_INPUT_PROCESSED = f"{DIR_INPUT}/processed"
 DIR_INPUT_RAW = f"{DIR_INPUT}/raw"
 FILEPATH_CLIMATOLOGY = f"/lustre/projetos/monan_atm/guilherme.mendonca/scratch/data/ERA5/mon/nc_climatology/climatology_in_monan_format.nc"
 # Other relevant input
-PREDICTION_MODEL = "gfs"
+PREDICTION_MODEL = "monan"
 REFERENCE_DATA = "gfs_analysis"
 DATE_INIT = "2026070100"
 DATE_FINAL = "2026070200"
@@ -77,26 +77,38 @@ def apply_pressure_level_mask_in_ref_and_prediction(ds_ref, ds_prediction, APPLY
         # Avoid calculating RMSE or ACC for surface_pressure if it exists in the dataset
         ds_ref = ds_ref.drop_vars("surface_pressure", errors="ignore")
         ds_prediction = ds_prediction.drop_vars("surface_pressure", errors="ignore")
+    
+    return ds_ref, ds_prediction
 
 # Construct filepaths for concatenated variable datasets
 time_window = "120"
 var_prediction_concat_filepath, var_ref_concat_filepath = get_prediction_ref_concat_filepath(time_window)
+
+print (var_prediction_concat_filepath)
+print (var_ref_concat_filepath)
+
 # Read concatenated variable datasets
 ds_var_prediction_concat = xr.open_dataset(var_prediction_concat_filepath, engine="netcdf4")
 ds_var_ref_concat = xr.open_dataset(var_ref_concat_filepath, engine="netcdf4")
+
+# Apply pressure validity mask to both datasets
+ds_var_ref_concat, ds_var_prediction_concat = apply_pressure_level_mask_in_ref_and_prediction(
+    ds_ref=ds_var_ref_concat,
+    ds_prediction=ds_var_prediction_concat,
+    APPLY_PRESSURE_LEVEL_VALIDITY_MASK=True
+)
 
 print ("prediction concat:")
 print (ds_var_prediction_concat)
 print (len(ds_var_prediction_concat.latitude))
 print (len(ds_var_prediction_concat.longitude))
-print (ds_var_prediction_concat["zgeo"].mean(dim="Time").sel(level="50000"))
-print (ds_var_prediction_concat["zgeo"].mean(dim="Time", keep_attrs=True).sel(level="50000"))
+print (ds_var_prediction_concat["zgeo"].mean(dim="Time").sel(level="50000").values)
 
 print ("ref concat:")
 print (ds_var_ref_concat)
 print (len(ds_var_ref_concat.latitude))
 print (len(ds_var_ref_concat.longitude))
-print (ds_var_ref_concat["zgeo"].mean(dim="Time").sel(level="50000"))
+print (ds_var_ref_concat["zgeo"].mean(dim="Time").sel(level="50000").values)
 
 # Read mapped climatology
 #ds_climatology = xr.open_dataset(vs_config.DIR_INPUT_PROCESSED+f"/climatology_mapped_to_ref_{vs_config.REFERENCE_DATA}.nc", engine="netcdf4")
@@ -106,6 +118,7 @@ print ("climatology:")
 print (len(ds_climatology.latitude))
 print (len(ds_climatology.longitude))
 print (ds_climatology.Time)
+print (ds_climatology.level)
 print (ds_climatology["zgeo"].sel(level="50000", Time="20200701"))
 
 # get month for calculation
@@ -113,8 +126,7 @@ month = utils.get_MM_str_from_YYYYMMDDHH_str(date_string=DATE_INIT)
 
 print (month)
 
-ds_acc = stats.anomaly_correlation_coefficient_standard(
-    var="zgeo", 
+ds_acc = stats.anomaly_correlation_coefficient_standard( 
     predictions=ds_var_prediction_concat,
     observations=ds_var_ref_concat,
     climatology=ds_climatology,

src/monan_analysis/stats.py
@@ -202,7 +202,7 @@ def anomaly_correlation_coefficient(predictions, observations, dim):
 
     return result
 
-def anomaly_correlation_coefficient_standard(var, predictions, observations, climatology, month_MM, ):
+def anomaly_correlation_coefficient_standard(predictions, observations, climatology, month_MM, ):
     """
     Calculate the anomaly correlation coefficient (ACC) for a specific variable between monthly 
     predictions and observations, using the standard definition employed in operational centers (1,2,3,4).
@@ -240,33 +240,37 @@ def anomaly_correlation_coefficient_standard(var, predictions, observations, cli
         or not isinstance(climatology, xr.Dataset):
         raise TypeError("Predictions, observations, and climatology must be xarray Datasets.")
     
-    if var not in predictions or var not in observations or var not in climatology:
-        raise ValueError(f"The variable '{var}' must exist in predictions, observations, and climatology datasets.")
-
     # Get monthly mean for predictions and observations
     ## Filter data for the specific month_MM
     predictions_filtered = predictions.sel(Time=predictions["Time"].dt.strftime("%m") == month_MM)
     observations_filtered = observations.sel(Time=observations["Time"].dt.strftime("%m") == month_MM)
-    ## Get monthly mean for the filtered predictions and observations
-    predictions_monthly = predictions_filtered[var].mean(dim="Time", keep_attrs=True)
-    observations_monthly = observations_filtered[var].mean(dim="Time", keep_attrs=True)
     
-    # Get climatology for that month (assuming 1991-2020 period, with climatology time stamps 
-    # written as 2020-MM-01)
-    climatology_monthly = climatology[var].sel(Time=f"2020-{month_MM}-01")
+    # Calculate ACC for each variable
+    for var in predictions.data_vars:
+        # Check if var exists in all datasets
+        if var not in predictions or var not in observations or var not in climatology:
+            raise ValueError(f"The variable '{var}' must exist in predictions, observations, and climatology datasets.")
 
-    # Compute anomalies
-    pred_anom = (predictions_monthly - climatology_monthly) - preprocess.spatial_mean(predictions_monthly - climatology_monthly)
-    obs_anom = (observations_monthly - climatology_monthly) - preprocess.spatial_mean(observations_monthly - climatology_monthly)
+        # Get monthly mean for the filtered predictions and observations
+        predictions_monthly = predictions_filtered[var].mean(dim="Time", keep_attrs=True)
+        observations_monthly = observations_filtered[var].mean(dim="Time", keep_attrs=True)
+        
+        # Get climatology for that month (assuming 1991-2020 period, with climatology time stamps 
+        # written as 2020-MM-01)
+        climatology_monthly = climatology[var].sel(Time=f"2020-{month_MM}-01")
 
-    # Create an empty dataset for the result
-    result = xr.Dataset()
+        # Compute anomalies
+        pred_anom = (predictions_monthly - climatology_monthly) - preprocess.spatial_mean(predictions_monthly - climatology_monthly)
+        obs_anom = (observations_monthly - climatology_monthly) - preprocess.spatial_mean(observations_monthly - climatology_monthly)
 
-    # Compute ACC and store in the result dataset
-    result[var] = preprocess.spatial_mean(pred_anom * obs_anom) / (
-        (preprocess.spatial_mean(pred_anom ** 2)) ** 0.5 *
-        (preprocess.spatial_mean(obs_anom ** 2)) ** 0.5
-    )
+        # Create an empty dataset for the result
+        result = xr.Dataset()
+
+        # Compute ACC and store in the result dataset
+        result[var] = preprocess.spatial_mean(pred_anom * obs_anom) / (
+            (preprocess.spatial_mean(pred_anom ** 2)) ** 0.5 *
+            (preprocess.spatial_mean(obs_anom ** 2)) ** 0.5
+        )
 
     return result
 